5f38bffc6c3f993ba45d0bafc3caf5c30c9e7303
Gautam Pranjal <gautam17pranjal@gmail.com>
2023-07-07 18:07:40 -0400

    adding GA4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,24 @@ function Document () {
         <Html lang="en" className="h-max bg-slate-50">
             <Head>
                 <link rel="icon" href="/logo.ico" sizes="192x192" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-5GPJQ4R');`,
+                    }}
+                />
             </Head>
             <body className="h-full">
             <Main />
             <NextScript />
+            <noscript
+                dangerouslySetInnerHTML={{
+                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GPJQ4R" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+                }}
+            />
             </body>
         </Html>
     )
